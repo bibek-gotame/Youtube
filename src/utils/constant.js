e9acdb8f852209d7626552cf38054b5e5efbbe21
bibek-gotame <bibekgotame011@gmail.com>
2024-04-30 15:01:47 +0545

    API key added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,3 @@
 export const buttonsData = ['All','Sports','Computer Science',' Web Development','A I','Machine Learning','Space']
-const api = 'https://www.googleapis.com/youtube/v3/videos'
+export const api =  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + import.meta.env.REACT_APP_YT_API
+ 
