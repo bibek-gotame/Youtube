d20616fe919ee1119ef2bd84710c9d0b996fb1b3
bibek-gotame <bibekgotame011@gmail.com>
2024-05-06 11:21:39 +0545

    api key changed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,10 +7,11 @@ export const buttonsData = [
     { id: 6, name: 'AI' },
     { id: 7, name: 'Machine Learning' },
     { id: 8, name: 'Space' }
-]; export const YTapi = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=" + import.meta.env.VITE_APP_YT_API
+];
+ export const YTapi = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + import.meta.env.VITE_APP_YT_API
 
 export const searchQueryAPI = 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
-export const searchDataAPI = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=20&key=' + import.meta.env.VITE_APP_YT_API + '&q='
+export const searchDataAPI = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&key=' + import.meta.env.VITE_APP_YT_API + '&q='
 export const commentsData = [
     {
         name: 'Youtube',
@@ -148,3 +149,4 @@ export const commentsData = [
         ]
     }
 ]
+
